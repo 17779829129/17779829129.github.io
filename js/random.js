2d6f2a2db105f8ed789d3c2eda395f9a1c3e7d22
17779829129 <17779829129@163.com>
2023-06-24 12:51:20 +0800

    Site updated: 2023-06-24 12:51:18

---
+++ js/random.js
@@ -0,0 +1,1 @@
+var posts=["2023/06/20/1/","2023/06/07/html/","2023/05/30/new/","2023/05/27/hello-world/","2023/06/24/2/"];function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
