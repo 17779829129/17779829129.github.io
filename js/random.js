4a3ef25260f3f67e66de44e51b7c303fc9acbbc9
17779829129 <17779829129@163.com>
2023-07-02 14:53:37 +0800

    Site updated: 2023-07-02 14:53:36

--- js/random.js
+++ js/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/06/20/1/","2023/06/24/2/","2023/06/25/3/","2023/06/27/4/","2023/07/02/5/","2023/05/27/hello-world/","2023/06/07/html/","2023/05/30/new/"];function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
+var posts=["2023/06/20/1/","2023/06/27/4/","2023/06/25/3/","2023/05/27/hello-world/","2023/06/07/html/","2023/06/24/2/","2023/07/02/5/","2023/05/30/new/"];function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
